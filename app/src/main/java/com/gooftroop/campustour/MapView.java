8768dc2f6805caa5dcb95b56b73cc89903a8446d
Austin Benson <benson@iastate.edu>
2015-02-17 21:19:34 -0600

    Austin clean commit, added code for swiping through images and working with Google Maps, Also added a CampusLocation Class to represent a location on campus

--- app/src/main/java/com/gooftroop/campustour/MapView.java
+++ app/src/main/java/com/gooftroop/campustour/MapView.java
@@ -1,27 +1,72 @@
 package com.gooftroop.campustour;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 public class MapView extends FragmentActivity implements GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap mMap;
 
     private Context curContext = this;
+
+    private boolean SHOW_BUILDING_BOUNDS = true;
+
+    /**
+     * Maintains a Master List of All Campus location and whether or not they have been visited
+     */
+    private HashMap<CampusLocation, Boolean> locations = new HashMap<CampusLocation, Boolean>();
+
+    DetailPageAdapter pageAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_view);
+        setContentView(R.layout.main_activity_images_layout);
+
+        List<Fragment> fragments = getFragments();
+
+        pageAdapter = new DetailPageAdapter(getSupportFragmentManager(), fragments);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.viewpager);
+        pager.setAdapter(pageAdapter);
+
+        //Set the class used for changing the animation for sliding images on the bottom
+        pager.setPageTransformer(true, new ZoomOutPageTransformer());
+
+        locations.put(createCooverHall(), false);
         setUpMapIfNeeded();
+    }
+
+    private List<Fragment> getFragments() {
+        List<Fragment> fList = new ArrayList<Fragment>();
+        fList.add(LocationImagesFragment.newInstance(R.drawable.coover_1, "A classroom in Coover Hall."));
+        fList.add(LocationImagesFragment.newInstance(R.drawable.coover_2, "Coover Hall was built in 1950."));
+        fList.add(LocationImagesFragment.newInstance(R.drawable.coover_3, "Coover Hall is home to the Department of Electrical and Computer Engineering."));
+        fList.add(LocationImagesFragment.newInstance(R.drawable.coover_4, "Coover Hall is home to many research laboratories."));
+        fList.add(LocationImagesFragment.newInstance(R.drawable.coover_5, "A student lounge space located on the 2nd floor."));
+        return fList;
     }
 
     @Override
@@ -79,16 +124,67 @@ public class MapView extends FragmentActivity implements GoogleMap.OnMarkerClick
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(42.0255, -93.6460)).title("Campanile"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(42.0255, -93.6460)).title("Campanile"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(42.024220,-93.651840)).title("Helser Hall");
-        mMap.addMarker(new MarkerOptions().position(new LatLng(42.023917,-93.650437)).title("Friley Hall");
-        mMap.addMarker(new MarkerOptions().position(new LatLng(42.028358, -93.650738)).title("Coover Hall"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(42.024220,-93.651840)).title("Helser Hall"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(42.023917,-93.650437)).title("Friley Hall"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(42.026163, -93.648340)).title("Beardshear Hall"));
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(42.028358, -93.650738)).title("Coover Hall"));
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(42.028358, -93.650738)).title("Coover Hall"));
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(42.028358, -93.650738)).title("Coover Hall"));
 
+        Set<CampusLocation> locationList = locations.keySet();
+
+        for (CampusLocation loc : locationList)
+        {
+            mMap.addMarker(new MarkerOptions().position(loc.getMarkerLocation()).title(loc.getName()));
+
+            if(SHOW_BUILDING_BOUNDS)
+            {
+                List<LatLngBounds> bnds = loc.getBuildingBoundsList();
+
+                for (LatLngBounds bnd : bnds)
+                {
+                    //mMap.addPolygon()
+                    Polyline line = mMap.addPolyline(new PolylineOptions()
+                            .add(bnd.northeast)
+                            .add(new LatLng(bnd.southwest.latitude, bnd.northeast.longitude))
+                            .add(bnd.southwest)
+                            .add(new LatLng(bnd.northeast.latitude, bnd.southwest.longitude))
+                            .add(bnd.northeast)
+                            .width(2)
+                            .color(Color.RED));
+                }
+            }
+        }
     }
+
+    private CampusLocation createCooverHall()
+    {
+        LatLngBounds.Builder bounds = LatLngBounds.builder();
+        bounds.include(new LatLng(42.02808, -93.65027));  //Southeast corner
+        bounds.include(new LatLng(42.028847, -93.65178)); //Northwest corner
+
+        ArrayList<LatLngBounds> boundsList = new ArrayList<LatLngBounds>();
+        boundsList.add(bounds.build());
+
+        return new CampusLocation("Coover Hall", new LatLng(42.028358, -93.650738), boundsList);
+    }
+
+    class DetailPageAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments;
+
+        public DetailPageAdapter (FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return this.fragments.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return this.fragments.size();
+        }
+    }
+
 }
