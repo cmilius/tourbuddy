c6f27afa733f0cb2c448cfa296f800a21c56af93
Matt Petron <mtpetron@iastate.edu>
2015-03-31 16:32:45 -0500

    Finished Upload Files code

--- tourbuddywebsite/tourbuddy/dashboard/code/php/uploadPictures.php
+++ tourbuddywebsite/tourbuddy/dashboard/code/php/uploadPictures.php
@@ -6,17 +6,20 @@
 	}
 	include("databaseconnection.php");
 	
-	var_dump($_POST);
+	//var_dump($_POST);
 
 	$buildingID = clean_input($_POST['buildingID']);
+	$buildingName = clean_input($_POST['buildingName']);
+	$count = clean_input($_POST['count']);
+	
 	//$buildingDescription = clean_input($_POST['buildingDescription']);
 	
 	
 	$fileName = uploadFile();
-	$fileName = $fileName.";";
+	//$fileName = $fileName.";";
 	echo($buildingName);
 	
-	$sql = "UPDATE buildings SET image_location=CONCAT(image_location, $fileName) 
+	$sql = "UPDATE buildings SET image_location=CONCAT(image_location, '$fileName') 
 			WHERE id='$buildingID'";
 	
 	
@@ -25,7 +28,7 @@
 	if($result === TRUE)
 	{
 		updateVersionNumber($conn);
-		header("Location:../buildings.php");
+		header("Location:../../buildings.php");
 	}
 	echo("ok");
 	mysqli_close($conn);
@@ -40,9 +43,10 @@
 	
 	function uploadFile()
 	{
-		for($i=0; $i < 3; $i++)
-		{
+			
 			global $buildingName;
+			global $count;
+			$buildingString = '';
 			$tempBuildingName = preg_replace('/\s+/', '_', $buildingName);
 			chdir("../");
 			$target_dir = "img/buildings/".$tempBuildingName."/";
@@ -52,14 +56,14 @@
 				//mkdir($target_dir);
 			//}
 			echo($target_dir);
-			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]);
+			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 			$uploadOk = 1;
 			
 			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-			echo($imageFileType);
+			//echo("Image File Type:". $imageFileType);
 			// Check if image file is a actual image or fake image
 			if(isset($_POST["submit"])) {
-				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"][$i]);
+				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 				if($check !== false) {
 					echo "File is an image - " . $check["mime"] . ".";
 					$uploadOk = 1;
@@ -74,7 +78,7 @@
 				$uploadOk = 0;
 			}
 			// Check file size
-			if ($_FILES["fileToUpload"]["size"][$i] > 500000) {
+			if ($_FILES["fileToUpload"]["size"] > 500000) {
 				echo "Sorry, your file is too large.";
 				$uploadOk = 0;
 			}
@@ -89,17 +93,18 @@
 				echo "Sorry, your file was not uploaded.";
 				// if everything is ok, try to upload file
 				} else {
-					$imageName = $tempBuildingName."_default.".$imageFileType;
+					$imageName = $tempBuildingName."_".$count.".".$imageFileType;
 					$target_file = $target_dir . $imageName;//basename($_FILES["fileToUpload"]["name"]);
-					if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file)) {
-						echo "The file ". basename( $_FILES["fileToUpload"]["name"][$i]). " has been uploaded.";
+					//$buildingString = $buildingString . $folder . $imageName . ";";
+					if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+						echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 					} else {
 						echo "Sorry, there was an error uploading your file.";
 					}
 				}
-			return $folder . $imageName;
-		}
+			return $folder . $imageName. ";";
 	}
+	
 	
 	function updateVersionNumber($conn){
 		$sql = "UPDATE version_number SET version = version + 1";
